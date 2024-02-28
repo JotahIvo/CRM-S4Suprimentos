5311017e66d7dd29a10775d7ec2c54434f31b7de
jota <j.pedro1415@ufu.br>
2024-02-28 10:50:19 -0300

    First Commit - Login and autentication

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, flash, redirect
+from src.login_autentication import login_autentication
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "senha123"
+
+
+@app.route("/")
+def home_page():
+    return render_template("html/login.html")
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    user_input = request.form.get('username')
+    password_input = request.form.get('password')
+    
+    autentication = login_autentication(user_input, password_input)
+    if autentication == "/":
+        flash('Usuário ou senha inválido(s)')
+        return redirect("/")
+    else:
+        if autentication == "admin":
+            return render_template("html/adminPage.html", username='Administrador')
+        else:
+            return render_template("html/userPage.html", username='Usuário')
+
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/login_autentication.py
@@ -0,0 +1,14 @@
+import json
+
+
+def login_autentication(user_input, password_input):
+    with open('users.json') as users:
+        list = json.load(users)
+    
+        if user_input == list[0]['username'] and password_input == list[0]['password']:
+            return user_input
+        else:
+            if user_input == list[1]['username'] and password_input == list[1]['password']:
+                return user_input
+            else:
+                return "/"
